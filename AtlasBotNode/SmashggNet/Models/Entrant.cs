6e9c73f8d1371034bb4ac3fe52642ca777dee3a8
Bart de Bever <bartdebever112@gmail.com>
2018-12-28 13:05:24 +0100

    Added documentation to models

--- AtlasBotNode/SmashggNet/Models/Entrant.cs
+++ AtlasBotNode/SmashggNet/Models/Entrant.cs
@@ -6,8 +6,14 @@ using Newtonsoft.Json;
 
 namespace SmashggNet.Models
 {
+    /// <summary>
+    /// A class to represent the user who has entered a tournament.
+    /// </summary>
     public class Entrant
     {
+        /// <summary>
+        /// Gets or sets the name of the entrant.
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
     }
